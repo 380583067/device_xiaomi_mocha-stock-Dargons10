30c8571618d1616189010319010601a159c81a63
apple <105837597+380583067@users.noreply.github.com>
2025-06-10 13:25:02 +0800

    Q

---
+++ shims/Android.mk
@@ -0,0 +1,70 @@
+LOCAL_PATH:= $(call my-dir)
+
+PROTOBUF_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libprotobuf-cpp-lite-3.9.1.so
+$(PROTOBUF_SYMLINK):
+	$(hide) mkdir -p $(TARGET_OUT_VENDOR)/lib
+	$(hide) ln -sf /system/lib/libprotobuf-cpp-lite.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(PROTOBUF_SYMLINK)
+
+## libshim_atomic
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := atomic.cpp
+LOCAL_MODULE := libshim_atomic
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := stdio_vsnprintf.cpp
+LOCAL_C_INCLUDES := \
+    bionic/libc \
+    bionic/libc/stdio \
+    bionic/libc/async_safe/include
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE := libs
+LOCAL_MODULE_TAGS := optional
+LOCAL_LDFLAGS_arm += -Wl,--version-script,$(LOCAL_PATH)/stdio_vsnprintf.arm.map
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := zygote_whitelist.cpp
+LOCAL_C_INCLUDES := frameworks/base/core/jni \
+                    system/core/base/include
+ifneq ($(ZYGOTE_WHITELIST_PATH_EXTRA),)
+    LOCAL_CFLAGS += -DPATH_WHITELIST_EXTRA=$(ZYGOTE_WHITELIST_PATH_EXTRA)
+endif
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE := libshim_zw
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+# libnvomxadaptor_shim
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libnvomxadaptor_shim.cpp
+LOCAL_SHARED_LIBRARIES := libui libgui libstagefright_foundation
+LOCAL_C_INCLUDES := frameworks/native/include frameworks/av/include frameworks/av/media/ndk/include
+LOCAL_CFLAGS := -Wno-unused-private-field
+LOCAL_MODULE := libnvomxadaptor_shim
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    camera_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libgui \
+    libui
+
+LOCAL_C_INCLUDES := \
+    frameworks/native/include
+
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ system_prop.mk
@@ -0,0 +1,154 @@
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sets Android Go default values for properties specific for mocha
+
+# Low RAM configuration - 调整为中等内存配置
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=false \
+    persist.traced.enable=1
+
+# LMK settings (调整为3GB RAM的配置参数)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.upgrade_pressure=40 \
+    ro.lmk.downgrade_pressure=60 \
+    ro.lmk.critical_pressure=80 \
+    ro.lmk.low=1000 \
+    ro.lmk.medium=2000 \
+    ro.lmk.critical=3000 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.use_minfree_levels=true \
+    ro.statsd.enable=true
+
+# Heap sizes (调整为3GB RAM的堆内存大小)
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.madvise-random=true
+
+# Compiler and optimization settings
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_PROPERTY_OVERRIDES += \
+    pm.dexopt.shared=quicken \
+    pm.dexopt.downgrade_after_inactive_days=10
+
+# Boot image profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Network stack
+PRODUCT_PACKAGES += InProcessNetworkStack
+
+# Minimize debug info
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# madvise random in ART to reduce page cache thrashing.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.madvise-random=true
+
+# ADB
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    persist.sys.usb.config=mtp,adb \
+    persist.sys.root_access=2 \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    ro.debuggable=1
+
+# AptX
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bt.enableAptXHD=true \
+    persist.service.btui.use_aptx=1 \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldacs
+
+# BT
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/system/vendor/etc/mocha_btmacaddr.txt
+
+# DIDIM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.didim.enable=1 \
+    persist.tegra.didim.video=5 \
+    persist.tegra.didim.normal=3
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# fastbootd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fastbootd.available=true
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196609 \
+    ro.sf.lcd_density=320 \
+    persist.tegra.compositor=surfaceflinger \
+    persist.tegra.decompression=cde-client \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1 \
+    ro.input.noresample=1 \
+    ro.zygote.disable_gl_preload=true \
+    persist.vendor.tegra.composite.policy=composite-yuv \
+    debug.hwc.force_gpu=1 \
+    persist.sys.ui.hw=false \
+    debug.hwui.use_buffer_age=false \
+    debug.hwui.renderer=opengl \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
+    debug.sf.disable_hwc=1
+
+# Lineage genuine
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.lineage.nofool=true
+
+# OMX
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
+
+# PBC
+PRODUCT_PROPERTY_OVERRIDES += \
+    pbc.enabled=0 \
+    pbc.log=0 \
+    pbc.board_power_threshold=20000 \
+    pbc.low_polling_freq_threshold=1000 \
+    pbc.rails=cpu,core,dram,gpu \
+    pbc.cpu.power=/sys/bus/i2c/devices/7-0045/power1_input \
+    pbc.cpu.cap=/dev/cpu_freq_max \
+    pbc.cpu.cap.af=/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies \
+    pbc.core.power=/sys/bus/i2c/devices/7-0043/power1_input \
+    pbc.dram.power=/sys/bus/i2c/devices/7-0049/power1_input \
+    pbc.gpu.power=/sys/bus/i2c/devices/7-004b/power1_input \
+    pbc.gpu.cap=/dev/gpu_freq_max \
+    pbc.gpu.cap.af=/sys/devices/platform/host1x/gk20a.0/devfreq/gk20a.0/available_frequencies
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.radio.noril=yes
+    
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1 \
+    wifi.interface=wlan0 \
+    ap.interface=wlan0 \
+    persist.tegra.nvmmlite = 1 \
+    persist.wlan.ti.calibrated = 0
