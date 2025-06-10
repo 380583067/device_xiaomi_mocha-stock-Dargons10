2fb6ee4c29f209c8775750a85a1068b9881f6423
apple <105837597+380583067@users.noreply.github.com>
2025-06-10 13:22:55 +0800

    Q

---
+++ initfiles/Android.mk
@@ -0,0 +1,126 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.tn8
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := fstab.tn8
+LOCAL_VENDOR_MODULE  := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.cal.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.cal.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.comms.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.comms.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.hdcp.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.hdcp.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.t124.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.t124.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tegra.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.tegra.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tlk.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.tlk.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tn8.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.tn8.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tn8.usb.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.tn8.usb.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tn8_common.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.tn8_common.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.ussrd.rc
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := init.ussrd.rc
+#LOCAL_VENDOR_MODULE := true
+#LOCAL_MODULE_RELATIVE_PATH := init/hw
+#include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := power.mocha.rc
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := power.mocha.rc
+#LOCAL_VENDOR_MODULE := true
+#LOCAL_MODULE_RELATIVE_PATH := init/hw
+#include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := power.tn8.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := power.tn8.rc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.tn8.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ueventd.tn8.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := ussrd.conf
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := ussrd.$(TARGET_TEGRA_VERSION).conf
+#LOCAL_VENDOR_MODULE := true
+#LOCAL_MODULE_RELATIVE_PATH := init/hw
+#include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.nvgpu_shims.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.nvgpu_shims.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init
+include $(BUILD_PREBUILT)
+
---
+++ libmocha/init_mocha.cpp
@@ -0,0 +1,72 @@
+/*
+   Copyright (c) 2020, The LineageOS Project
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define LOG_TAG "mocha_init"
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+#include <sys/sysinfo.h>
+
+#include "property_service.h"
+
+char const* heapstartsize;
+char const* heapgrowthlimit;
+char const* heapsize;
+char const* heapminfree;
+
+void get_dalvik_heap_props()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > 2048ull * 1024 * 1024) {
+        LOG(VERBOSE) << "3Gb RAM device";
+        heapstartsize = "8m";
+        heapgrowthlimit = "288m";
+        heapsize = "768m";
+        heapminfree = "512k";
+    } else {
+        LOG(VERBOSE) << "2Gb RAM device";
+        heapstartsize = "16m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heapminfree = "2m";
+    }
+}
+
+void vendor_load_properties()
+{
+    get_dalvik_heap_props();
+
+    android::init::property_set("dalvik.vm.heapstartsize", heapstartsize);
+    android::init::property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    android::init::property_set("dalvik.vm.heapsize", heapsize);
+    android::init::property_set("dalvik.vm.heaptargetutilization", "0.75");
+    android::init::property_set("dalvik.vm.heapminfree", heapminfree);
+    android::init::property_set("dalvik.vm.heapmaxfree", "8m");
+}
