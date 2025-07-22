2fc54bd952eac1002601aebde8ef584794586815
apple <105837597+380583067@users.noreply.github.com>
2025-07-22 20:05:43 +0800

    Add files via upload

---
+++ camera/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp \
+    Camera3Wrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog  libcamera_client libutils libgui libhidltransport libcamera_metadata libsensor android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
+
+LOCAL_C_INCLUDES += \
+    frameworks/native/include \
+    frameworks/native/include/media/openmax \
+    frameworks/native/libs/nativebase/include \
+    system/core/include \
+    system/core/base/include \
+    system/media/camera/include \
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
+
+LOCAL_32_BIT_ONLY := true
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
